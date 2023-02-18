697c188cc2ec1263da0834d03755b9f90f65ee33
Sanika  Rangayyan <105531459+SanikaRangayyan@users.noreply.github.com>
2023-02-18 21:37:30 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World by PES1UG20CS901\n";
+  cout << "Hello, Jenkins by PES1UG20CS901\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
