a6b5888e5d08c2851c79674463af7d9039210c49
Sanika  Rangayyan <105531459+SanikaRangayyan@users.noreply.github.com>
2023-02-18 22:37:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World by PES1UG20CS901\n";
   cout << "Hello, Jenkins by PES1UG20CS901\n";
-  cout << "I have successfully built and run\n";
+  cout << "This is PES1UG20CS901- C section\n";
   return 0;
 }
